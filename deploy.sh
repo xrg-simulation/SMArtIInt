f0e4fb97f00311d693eacf9bb4011136afb8c071
Robert Flesch <flesch@xrg-simulation.de>
2024-04-23 09:11:34 +0200

    Finalize and test deploy.sh - modify CMakeLists.txt to use CMAKE_BINARY_DIR

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,9 @@ date=$(git show -s --format=%ci)
 rm -r _deploy/
 mkdir _deploy/
 
-Robocopy.exe SMArtIInt ./_deploy/SMArtIInt //e //xd ./SMArtIInt/Resources/ExampleNeuralNets/venv/ ./SMArtIInt/Resources/ExampleNeuralNets/NNHeatTransfer/ht_log ./SMArtIInt/Resources/ExampleNeuralNets/NNHeatTransfer/large* ./SMArtIInt/Resources/ExampleNeuralNets/NNHeatTransfer/small* ./SMArtIInt/Resources/ExampleNeuralNets/.idea
+# in case we run on a windows machine we do not have rsync - therefore use find in combination with mkdir and cp to copy the required files
+find ./SMArtIInt/* -type d ! \( -path "*venv*" -or -path "*idea*" -or -path *_log* -or -name data_*.pckl \)  -exec mkdir -p ./_deploy/{} \;
+find ./SMArtIInt/* -type f ! \( -path "*venv*" -or -path "*idea*" -or -path *_log* -or -name data_*.pckl \)  -exec cp {} ./_deploy/{} \;
 
 sed -i -e "s/%build%/$build/g" ./_deploy/SMArtIInt/package.mo
 sed -i -e "s/%version%/$version/g" ./_deploy/SMArtIInt/package.mo
@@ -19,9 +21,7 @@ sed -i -e "s/%date%/$date/g" ./_deploy/SMArtIInt/package.mo
 
 #clean up
 rm ./_deploy/SMArtIInt/Resources/Library/win64/*
-cp ./SMArtIInt/Resources/Library/win64/tensorflowlite_c.dll ./_deploy/SMArtIInt/Resources/Library/win64/
 rm ./_deploy/SMArtIInt/Resources/Library/win64/*
-cp ./SMArtIInt/Resources/Library/linux64/tensorflowlite_c.so ./_deploy/SMArtIInt/Resources/Library/linux64/
 
 # Windows build
 #cmake -S ./CSource -B ./_deploy/_cmake/ -G "MinGW Makefiles"
@@ -36,10 +36,19 @@ cd ./_deploy/_cmake_wsl
 wsl cmake --build . --config Release
 cd ../../
 
+# copy additional required libs
+cp ./SMArtIInt/Resources/Library/win64/tensorflowlite_c.dll ./_deploy/SMArtIInt/Resources/Library/win64/
+cp ./SMArtIInt/Resources/Library/linux64/libtensorflowlite_c.so ./_deploy/SMArtIInt/Resources/Library/linux64/
 
 # pack everything
 cp ./README.md ./_deploy/
 cp LICENSE ./_deploy/
 cd _deploy/
-zip -r ../SMArtIInt_$version"_"$build.zip ./SMArtIInt/
+# clean the zip file first
+zip -d ../SMArtIInt_$version"_"$build.zip
+zip -r ../SMArtIInt_$version"_"$build.zip ./SMArtIInt README.md LICENSE
 
+echo "Press any key to continue..."
+# -s: Do not echo input coming from a terminal
+# -n 1: Read one character
+read -s -n 1
